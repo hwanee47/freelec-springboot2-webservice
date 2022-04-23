22a0bd81c227f24da418890a3a118532b7e3b2c4
hwanee47 <>
2022-04-24 06:06:24 +0900

    chapter02 ~ 04 게시글CRUD와 테스트 코드 작성

---
+++ src/main/java/com/hwanee/book/springboot/web/PostsApiController.java
@@ -0,0 +1,36 @@
+package com.hwanee.book.springboot.web;
+
+import com.hwanee.book.springboot.service.posts.PostsService;
+import com.hwanee.book.springboot.web.dto.PostsResponseDto;
+import com.hwanee.book.springboot.web.dto.PostsSaveRequestDto;
+import com.hwanee.book.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto postsSaveRequestDto){
+        return postsService.save(postsSaveRequestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id){
+        return postsService.findById(id);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id){
+        postsService.delete(id);
+        return id;
+    }
+}
